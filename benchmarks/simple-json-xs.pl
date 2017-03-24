023c3942be65688e5d9c98e552bc7cc6250b3306
Stevan Little <stevan.little@booking.com>
2017-03-24 17:40:16 +0100

    some misc stuf

--- benchmarks/simple-json-xs.pl
+++ benchmarks/simple-json-xs.pl
@@ -8,13 +8,15 @@ use Proc::ProcessTable;
 
 use JSON::XS;
 
+use constant VERBOSE => $ENV{VERBOSE} // 0;
+
 my $proc_table = Proc::ProcessTable->new;
 my $json       = JSON::XS->new;
 
 $| = 1;
 
-print(('-' x 60), "\n");
-printf "START : %d\n" => (grep { $_->pid eq $$ } @{ $proc_table->table })[0]->rss;
+print(('-' x 60), "\n") if VERBOSE;
+printf("START : %d\n" => (grep { $_->pid eq $$ } @{ $proc_table->table })[0]->rss) if VERBOSE;
 my $output = $json->decode( join '' => <ARGV> );
-printf "END   : %d\n" => (grep { $_->pid eq $$ } @{ $proc_table->table })[0]->rss;
-print(('-' x 60), "\n");
+printf("END   : %d\n" => (grep { $_->pid eq $$ } @{ $proc_table->table })[0]->rss) if VERBOSE;;
+print(('-' x 60), "\n") if VERBOSE;;
