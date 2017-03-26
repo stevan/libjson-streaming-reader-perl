c78d425669e0816967cad8b4452c189f98bb17a1
Stevan Little <stevan.little@gmail.com>
2017-03-26 09:26:39 +0200

    finally, a legit use for dualvar :P

--- lib/JSON/Streaming/Tokens.pm
+++ lib/JSON/Streaming/Tokens.pm
@@ -3,23 +3,25 @@ package JSON::Streaming::Tokens;
 use strict;
 use warnings;
 
+use Scalar::Util;
+
 our $VERSION = '0.01';
 
 our ( @EXPORTS, %TOKEN_MAP, %TOKEN_LKUP );
 
 BEGIN {
     %TOKEN_MAP = (
-        START_OBJECT   => 0b00000000001,
-        END_OBJECT     => 0b00000000010,
-        START_ARRAY    => 0b00000000100,
-        END_ARRAY      => 0b00000001000,
-        START_PROPERTY => 0b00000010000,
-        END_PROPERTY   => 0b00000100000,
-        ADD_STRING     => 0b00001000000,
-        ADD_NUMBER     => 0b00010000000,
-        ADD_BOOLEAN    => 0b00100000000,
-        ADD_NULL       => 0b01000000000,
-        ERROR          => 0b10000000000,
+        START_OBJECT   => Scalar::Util::dualvar( 0b00000000001, 'START_OBJECT'   ),
+        END_OBJECT     => Scalar::Util::dualvar( 0b00000000010, 'END_OBJECT'     ),
+        START_ARRAY    => Scalar::Util::dualvar( 0b00000000100, 'START_ARRAY'    ),
+        END_ARRAY      => Scalar::Util::dualvar( 0b00000001000, 'END_ARRAY'      ),
+        START_PROPERTY => Scalar::Util::dualvar( 0b00000010000, 'START_PROPERTY' ),
+        END_PROPERTY   => Scalar::Util::dualvar( 0b00000100000, 'END_PROPERTY'   ),
+        ADD_STRING     => Scalar::Util::dualvar( 0b00001000000, 'ADD_STRING'     ),
+        ADD_NUMBER     => Scalar::Util::dualvar( 0b00010000000, 'ADD_NUMBER'     ),
+        ADD_BOOLEAN    => Scalar::Util::dualvar( 0b00100000000, 'ADD_BOOLEAN'    ),
+        ADD_NULL       => Scalar::Util::dualvar( 0b01000000000, 'ADD_NULL'       ),
+        ERROR          => Scalar::Util::dualvar( 0b10000000000, 'ERROR'          ),
     );
 
     %TOKEN_LKUP = reverse %TOKEN_MAP;
